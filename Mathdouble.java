73cbe5254ce9faed61ddad30dd5f10765ebe79f7
ranafaisal1746 <105580744+ranafaisal1746@users.noreply.github.com>
2022-05-20 02:53:51 -0700

    Add files via upload
    assignment No 1
    chapter 4

---
+++ Mathdouble.java
@@ -0,0 +1,16 @@
+ public class  Mathdouble
+     {   public static void main(String args[]) 
+              {
+                   System.out.println("\nFloating Point Arithmetic");
+                             double da = 1 + 1;
+                             double db = da * 3;
+                             double dc = db / 4;
+                             double dd = dc - da;
+                             double de = -dd;
+                                System.out.println("da = " + da);
+                                System.out.println("db = " + db);
+                                System.out.println("dc = " + dc);
+                                System.out.println("dd = " + dd);
+                                System.out.println("de = " + de);
+               }
+     }
